ea9bb9b5db20eb2da1ecbbd87ce2260fac285f60
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-07-14 19:59:12 +0530

    Time: 16 ms (16.07%), Space: 5.9 MB (90.89%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long int sum = 0;
+        int count = 0;
+        for(int i=1; sum<n; i++)
+        {
+            sum += i;
+            if(sum<=n)
+                count++;
+        }
+        return count;
+    }
+};
