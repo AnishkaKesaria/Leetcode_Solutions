e0c1450c08efcd4f299e297b4b27da4342989ef1
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-08-01 19:34:09 +0530

    Time: 23 ms (5.12%), Space: 16.8 MB (5.91%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,73 @@
+
+class TrieNode{
+    public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal;
+    int childCount;
+
+    TrieNode(char d)
+    {
+        this->data = d;
+        for(int i=0; i<26; i++)
+            this->children[i] = NULL;
+        this->childCount = 0;
+        this->isTerminal = false;
+    }
+};
+
+void insertWord(TrieNode* root, string word)
+{
+    if(word.length()==0)
+    {
+        root->isTerminal = true;
+        return;
+    }
+
+    char ch = word[0];
+    int index = ch - 'a';
+    TrieNode* child;
+
+    if(root->children[index] != NULL)
+        child = root->children[index];
+    else
+    {
+        child = new TrieNode(ch);
+        root->children[index] = child; 
+        root->childCount += 1;
+    }
+
+    insertWord(child, word.substr(1));
+    return;
+}
+
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
-        for(int i=0; i<strs[0].size(); i++)
+    void findLCP(string word, string &ans, TrieNode* root)
+    {
+        for(int i=0; i<word.length(); i++)
         {
-            char ch = strs[0][i];
-            for(int j = 1; j<strs.size(); j++)
+            char ch = word[i];
+            if(root->childCount == 1 && root->isTerminal == false)
             {
-                if( strs[j].size()<i || strs[j][i] != ch)
-                    return ans;
+                int index = ch-'a';
+                root = root->children[index];
+                ans += ch;
             }
-            ans += ch;
+            else
+                break;
         }
+        return;
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        TrieNode* root = new TrieNode('-');
+        for(int i=0; i<strs.size(); i++)
+            insertWord(root, strs[i]);
+
+        string ans = "";
+        string first = strs[0];
+        findLCP(first, ans, root);
         return ans;
     }
 };
