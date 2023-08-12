88421861d32d1c6d92e38ad081abff9b672104a9
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-08-12 17:39:18 +0530

    Time: 0 ms (100.00%), Space: 6 MB (23.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    //Bottom Up approach
+    //Space Optimisation
     int fib(int n) {
         if(n==0 || n==1)
             return n;
-        vector<int> dp(n+1, -1);
-        dp[0] = 0, dp[1] = 1;
+        int prev2 = 0;
+        int prev1 = 1;
+        int curr;
         for(int i=2; i<=n; i++)
-            dp[i] = dp[i-1] + dp[i-2];
-        return dp[n];
+        {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;    
     }
 };
