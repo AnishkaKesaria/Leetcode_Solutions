913ce1dfe658bf6392405ba93b3bfe557625fac4
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-08-13 10:53:48 +0530

    Time: 57 ms (93.71%), Space: 14.4 MB (70.89%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int coinChangeDP(vector<int>& coins, int amount, vector<int> &dp) {
-        if(amount==0)
-            return 0;
-        if(amount<0)
-            return INT_MAX;
-        
-        if(dp[amount]!=-1)
-            return dp[amount];
-        
-        int ans = INT_MAX;
-        for(int i=0; i<coins.size(); i++)
+    int coinChangeDP(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] = 0;
+        for(int i=1; i<=amount; i++)
         {
-            int tempAns = coinChangeDP(coins, amount-coins[i], dp);
-            if(tempAns!=INT_MAX)
-                ans = min(tempAns+1, ans);
+            int ans = INT_MAX;
+            for(int j=0; j<coins.size(); j++)
+            {
+                if(i-coins[j]>=0)
+                {
+                    int tempAns = dp[i-coins[j]];
+                    if(tempAns!=INT_MAX)
+                        ans = min(ans, tempAns+1);
+                }
+            }
+            dp[i] = ans;
         }
-        
-        dp[amount] = ans;
         return dp[amount];
     }
     
     int coinChange(vector<int> &coins, int amount)
     {
-        vector<int> dp(amount+1, -1);
-        int ans = coinChangeDP(coins, amount, dp);
+        int ans = coinChangeDP(coins, amount);
         if(ans==INT_MAX)
             return -1;
         return ans;
-        
     }
 };
