8895d45c8bfad252a803a2da6a5db78552d4078d
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-08-13 16:01:55 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (15.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -29,11 +29,29 @@ public:
         dp[n] = max(include, exclude);
         return dp[n];
     }
+    
+    int solveUsingTab(vector<int>&nums, int n)
+    {
+        vector<int> dp(n+1, 0);
+        dp[0] = nums[0];
+        
+        for(int i=1; i<=n; i++)
+        {
+            int temp = 0;
+            if(i-2>=0)
+                temp = dp[i-2];
+            int include = temp + nums[i];
+            int exclude = dp[i-1];
+            
+            dp[i] = max(include, exclude);
+        }
+        return dp[n];
+    }
 
     int rob(vector<int>& nums) {
         int n = nums.size()-1;
-        vector<int> dp(n+1, -1); 
-        int ans = solveUsingMem(nums,n, dp);
+        // vector<int> dp(n+1, -1); 
+        int ans = solveUsingTab(nums,n);
         return ans;
     }
 };
