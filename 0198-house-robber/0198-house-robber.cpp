a1a6ea70b10921cab92776c1df82e0a30635fc7d
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-08-13 16:13:22 +0530

    Time: 3 ms (37.98%), Space: 7.7 MB (74.40%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -47,11 +47,38 @@ public:
         }
         return dp[n];
     }
+    
+    int solveUsingSpaceOpt(vector<int> &nums, int n)
+    {
+        if(n==0)
+            return nums[0];
+        
+        int prev2 = 0;
+        int prev1 = nums[0];
+        
+        int curr = 0;
+        
+        for(int i=1; i<=n; i++)
+        {
+            int temp = 0;
+            if(i-2>=0)
+                temp = prev2;
+            int include = temp + nums[i];
+            int exclude = prev1;
+            
+            curr = max(include, exclude);
+            
+            prev2 = prev1;
+            prev1 = curr;
+            
+        }
+        return curr;
+    }
 
     int rob(vector<int>& nums) {
         int n = nums.size()-1;
         // vector<int> dp(n+1, -1); 
-        int ans = solveUsingTab(nums,n);
+        int ans = solveUsingSpaceOpt(nums,n);
         return ans;
     }
 };
