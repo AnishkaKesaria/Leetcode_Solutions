f05a18f648ee4b8c32295a8f7c62076671ac31bd
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-09-15 00:30:40 +0530

    Time: 20 ms (83.49%), Space: 8.8 MB (26.63%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    int MOD = 1000000007;
+public:
+    int numRollsMem(int n, int k, int target, vector<vector<int>> &dp)
+    {
+        if(target==0 && n==0)
+            return 1;
+        if(target==0 && n!=0)
+            return 0;
+        if(target<0)
+            return 0;
+        if(n==0)
+            return 0;
+
+        if(dp[n][target]!= -1)
+            return dp[n][target];
+
+        long long int ans = 0;
+        for(int i=1; i<=k; i++)
+        {
+            ans += numRollsMem(n-1, k, target-i, dp);
+        }
+        dp[n][target] = ans%MOD;
+        return dp[n][target];
+    }
+
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp (n+1, vector<int> (target+1, -1));
+        return numRollsMem(n,k,target,dp);
+    }
+};
