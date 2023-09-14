61914d7835069bb9467062e3166434954023365a
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-09-14 22:27:35 +0530

    Time: 224 ms (63.68%), Space: 10.8 MB (87.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -90,6 +90,30 @@ public:
         return curr[target];
     }
 
+    bool isSubsetSumSOO(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+
+        // vector<int> curr(target+1, 0);
+        vector<int> next(target+1, 0);
+        next[0] = 1;
+        
+        for(int index = n-1; index>=0; index--)
+        {
+            for(int currTarget = target; currTarget>=0; currTarget--)
+            {
+                bool include = 0;
+                if(currTarget-nums[index]>=0)
+                    include = next[currTarget-nums[index]];
+                bool exclude = next[currTarget];
+
+                next[currTarget] = (include||exclude);
+            }
+        }
+
+        return next[target];
+    }
+
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(int i=0; i<nums.size(); i++)
@@ -100,6 +124,6 @@ public:
         // int currIndex = 0;
         // vector<vector<int>> dp(target+1, vector<int> (nums.size(), -1));
         // return isSubsetSumMem(nums, target);
-        return isSubsetSumSO(nums, target);
+        return isSubsetSumSOO(nums, target);
     }
 };
