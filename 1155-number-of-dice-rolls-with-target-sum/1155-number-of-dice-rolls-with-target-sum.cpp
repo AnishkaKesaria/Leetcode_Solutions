8dc57191f045e7e9b55f5466b82c0356bce8e36c
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-09-16 18:15:12 +0530

    Time: 26 ms (61.32%), Space: 8.8 MB (26.33%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -24,8 +24,49 @@ public:
         return dp[n][target];
     }
 
+    int numRollsTab(int n, int k, int target)
+    {
+        vector<vector<int>> dp(n+1, vector<int> (target+1, 0));
+        dp[0][0] = 1;
+
+        for(int index=1; index<=n; index++)
+        {
+            for(int t=1; t<=target; t++)
+            {
+                long long int ans = 0;
+                for(int i=1; i<=k; i++)
+                {
+                    if(t-i>=0)
+                        ans += dp[index-1][t-i];
+                }
+                dp[index][t] = ans%MOD;
+            }
+        }
+
+        return dp[n][target];
+    }
+
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp (n+1, vector<int> (target+1, -1));
-        return numRollsMem(n,k,target,dp);
+        // vector<vector<int>> dp (n+1, vector<int> (target+1, -1));
+        // return numRollsMem(n,k,target,dp);
+        // vector<vector<int>> dp(n+1, vector<int> (target+1, 0));
+        // dp[0][0] = 1;
+
+        // for(int index=1; index<=n; index++)
+        // {
+        //     for(int t=1; t<=target; k++)
+        //     {
+        //         long long int ans = 0;
+        //         for(int i=1; i<=k; i++)
+        //         {
+        //             if(t-i>=0)
+        //                 ans += dp[index-1][t-i];
+        //         }
+        //         dp[index][t] = ans%MOD;
+        //     }
+        // }
+
+        // return dp[n][target];
+        return numRollsTab(n,k,target);
     }
 };
