6376f3278c75983b6ab44f8e221bfd8fe7c3bae1
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-09-22 18:25:52 +0530

    Time: 87 ms (66.99%), Space: 8.8 MB (5.08%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -35,12 +35,85 @@ public:
         return dp[start][end];
     }
 
+    // int solveUsingTab(int n)
+    // {
+    //     vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+
+    //     for(int start = 1; start<=n; start++)
+    //     {
+    //         for(int end = n; end>=1; end--)
+    //         {
+    //             int ans = INT_MAX;
+
+    //             for(int i=start; i<=end; i++)
+    //             {
+    //                 ans = min(ans, i+ max(dp[start][i-1], dp[i+1][end]));
+    //             }
+
+    //             dp[start][end] = ans;
+    //         }
+    //     }
+
+    //     return dp[n][1];
+    // }
+
+    int solveUsingTab(int n)
+    {
+        vector<vector<int>> dp (n+1, vector<int> (n+1, 0));
+
+        // for(int start=1; start<=n; start++)
+        // {
+        //     for(int end=n; end>=1; end--)
+        //     {
+        //         if(start<end)
+        //         {
+        //             int ans = INT_MAX;
+
+        //             for(int i=start; i<=end; i++)
+        //             {
+        //                 if(i-1>=1 && i+1<=n)
+        //                     ans = min(ans, i + max(dp[start][i-1], dp[i+1][end]));
+        //             }
+
+        //             dp[start][end] = ans;
+        //         }
+        //     }
+        // }
+
+        for(int start = n; start>=1; start--)
+        {
+            for(int end = 1; end<=n; end++)
+            {
+                if(start<end)
+                {
+                    int ans = INT_MAX;
+
+                    for(int i=start; i<=end; i++)
+                    {
+                        if(i-1>=1 && i+1<=n)
+                            ans = min(ans, i + max(dp[start][i-1], dp[i+1][end]));
+                    }
+
+                    dp[start][end] = ans;
+
+                    if(ans==INT_MAX)
+                        dp[start][end] = 1;
+                }
+                else
+                    dp[start][end] = 0;
+            }
+        }
+
+        return dp[1][n];
+    }
+
     int getMoneyAmount(int n) {
         int start = 1;
         int end = n;
         // int ans = solveUsingRec(start, end);
-        vector<vector<int>> dp (n+1, vector<int> (n+1, INT_MIN));
-        int ans = solveUsingMem(start, end, dp);
+        // vector<vector<int>> dp (n+1, vector<int> (n+1, INT_MIN));
+        // int ans = solveUsingMem(start, end, dp);
+        int ans = solveUsingTab(n);
         return ans;
     }
 };
