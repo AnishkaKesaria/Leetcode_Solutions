6a75e8dcf01e5e868d0d1d326dc5cc90a3c11e9b
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-09-28 16:34:08 +0530

    Time: 36 ms (33.02%), Space: 14.7 MB (5.89%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -43,6 +43,32 @@ public:
         return dp[left][right];
     }
 
+    int solveUsingTab(map<pair<int,int>, int> maxi, vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+
+        for(int left= n-1; left>=0; left--)
+        {
+            for(int right = 0; right<n; right++)
+            {
+                int ans = INT_MAX;
+                for(int i=left; i<right; i++)
+                {
+                    if(i+1<=arr.size()-1)
+                        ans = min(ans, (maxi[{left,i}]*maxi[{i+1, right}]) + dp[left][i] + dp[i+1][right] );
+                }
+
+                if(ans==INT_MAX)
+                    ans = 0;
+
+                dp[left][right] = ans;
+            }
+        }
+        return dp[0][n-1];
+
+    } 
+
     int mctFromLeafValues(vector<int>& arr) {
 
         map< pair<int,int>, int> maxi;
@@ -63,7 +89,7 @@ public:
         int left = 0;
         int right = arr.size()-1;
 
-        int ans = solveUsingMem(left, right, dp, maxi, arr);
+        int ans = solveUsingTab(maxi, arr);
 
         // int ans = solve(left, right, maxi, arr);
         return ans;
