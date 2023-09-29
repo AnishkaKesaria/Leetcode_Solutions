55c58065041378a181a729da099f5d3fb4750e48
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-09-29 14:49:07 +0530

    Time: 31 ms (83.04%), Space: 6.7 MB (90.10%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -63,11 +63,38 @@ public:
         return dp[0][0];
     }
 
+    int solveSO(string &text1, string &text2)
+    {
+        int k = max(text1.size(), text2.size());
+
+        vector<int> curr (k+1, 0);
+        vector<int> next (k+1, 0);
+
+        for(int i=text1.size()-1; i>=0; i--)
+        {
+            for(int j=text2.size()-1; j>=0; j--)
+            {
+                int ans;
+
+                if(text1[i]==text2[j])
+                    ans = 1+next[j+1];
+                else
+                    ans = max(curr[j+1],next[j]);
+
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+
+        return next[0];
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
         // vector<vector<int>> dp(n, vector<int> (m,-1));
         // int ans = solveUsingMem(text1, text2, 0, 0, dp);
-        int ans = solveUsingTab(text1, text2);
+        // int ans = solveUsingTab(text1, text2);
+        int ans = solveSO(text1, text2);
         // int ans = solveUsingRec(text1, text2, 0, 0);
         return ans;
     }
