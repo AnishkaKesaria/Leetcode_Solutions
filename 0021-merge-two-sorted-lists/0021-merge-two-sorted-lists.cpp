dc72a08b1aba3819db7f8191a6cd6421488235d1
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-10-02 00:31:29 +0530

    Time: 10 ms (15.15%), Space: 15.1 MB (24.96%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,65 +10,22 @@
  */
 class Solution {
 public:
-    void insert(ListNode* &head, ListNode* &tail, int val)
-    {
-        ListNode* newNode = new ListNode(val);
-
-        if(head==NULL)
-        {
-            head = newNode;
-            tail = newNode;
-            return;
-        }
-
-        if(head==tail)
-        {
-            tail->next = newNode;
-            tail = tail->next;
-            head->next = tail;
-            return;
-        }
-
-        tail->next = newNode;
-        tail = tail->next;
-        return;
-    }
-
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* head = NULL;
-        ListNode* tail = NULL;
-
         if(list1==NULL)
             return list2;
         
         if(list2==NULL)
             return list1;
 
-        while(list1!=NULL && list2!=NULL)
+        if(list1->val <= list2->val)
         {
-            if(list1->val<=list2->val)
-            {
-                insert(head, tail, list1->val);
-                list1 = list1->next;
-            }
-            else
-            {
-                insert(head, tail, list2->val);
-                list2 = list2->next;
-            }
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
         }
-
-        while(list1!=NULL)
+        else
         {
-            insert(head, tail, list1->val);
-            list1 = list1->next;
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
-
-        while(list2!=NULL)
-        {
-            insert(head, tail, list2->val);
-            list2 = list2->next;
-        }
-        return head;
     }
 };
