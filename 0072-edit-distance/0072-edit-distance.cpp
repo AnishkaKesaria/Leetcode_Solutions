d72c2e52ccfd538bef6bc562c2ba25c0d361b5d6
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-10-02 22:46:07 +0530

    Time: 15 ms (38.15%), Space: 12.2 MB (19.21%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -68,6 +68,74 @@ public:
         return dp[i][j];
     }
 
+    int solveUsingTab(string word1, string word2)
+    {
+        int k = max(word1.size(), word2.size());
+        // vector<vector<int>> dp(k+2, vector<int> (k+2, 0));
+        vector<vector<int>> dp(word1.size()+1, vector<int> (word2.size()+1, 0));
+
+        for(int j=0; j<word2.size(); j++)
+        {
+            dp[word1.size()][j] = word2.size()-j;
+        }
+
+        for(int i=0; i<word1.size(); i++)
+        {
+            dp[i][word2.size()] = word1.size()-i;
+        }
+
+        // int n1 = word1.size();
+        // int n2 = word2.size();
+
+        // for(int i=n1; i<=k; i++)
+        // {
+        //     for(int j=0; j<=k; j++)
+        //     {
+        //         if(j>=n2)
+        //         {
+        //             dp[i][j] = 0;
+        //         }
+        //         else
+        //         {
+        //             dp[i][j] = n2-j;
+        //         }
+        //     }
+        // }
+
+        // for(int j=n2; j<=k; j++)
+        // {
+        //     for(int i=0; i<=k; i++)
+        //     {
+        //         if(i>=n1)
+        //             dp[i][j] = 0;
+        //         else
+        //             dp[i][j] = n1-i;
+        //     }
+        // }
+
+        for(int i=word1.size()-1; i>=0; i--)
+        {
+            for(int j=word2.size()-1; j>=0; j--)
+            {
+                int ans;
+
+                if(word1[i]==word2[j])
+                    ans = dp[i+1][j+1];
+
+                else
+                {
+                    int insertOp = 1 + dp[i][j+1];
+                    int deleteOp = 1 + dp[i+1][j];
+                    int replaceOp = 1 + dp[i+1][j+1];
+                    ans = (min(insertOp, min(deleteOp, replaceOp)));
+                }
+
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+
     
 
     int minDistance(string word1, string word2) {
@@ -80,7 +148,9 @@ public:
         int k = max(word1.size(), word2.size());
 
         vector<vector<int>> dp(k+1, vector<int> (k+1, -1));
-        int ans = solveUsingMem(word1, word2, i, j, dp);
+        // int ans = solveUsingMem(word1, word2, i, j, dp);
+        int ans = solveUsingTab(word1, word2);
+
 
         // int ans = solveUsingRec(word1, word2, i, j);
         return ans;
