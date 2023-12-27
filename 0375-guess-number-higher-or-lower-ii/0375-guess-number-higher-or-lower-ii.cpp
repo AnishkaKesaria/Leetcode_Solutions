4c3bfe90281c42d379636c46224c95c46dace824
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-12-27 13:58:00 +0530

    Time: 81 ms (74.08%), Space: 8.6 MB (59.22%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    int solTab(int n)
+    {
+        vector<vector<int>> dp (n+2, vector<int>(n+1, 0));
+
+        for(int start=n; start>=1; start--)
+        {
+            for(int end = 1; end<=n; end++)
+            {
+
+                if(start>=end)
+                    continue;
+
+                int ans = INT_MAX;
+                for(int i=start; i<=end; i++)
+                {
+                    ans = min(ans, i + max(dp[start][i-1], dp[i+1][end]));
+                }
+                dp[start][end] = ans;
+            }
+        }
+        return dp[1][n];
+    }
+
     int solRec(int start, int end, vector<vector<int>> &dp)
     {
         if(start>=end)
@@ -19,8 +42,9 @@ public:
     }
 
     int getMoneyAmount(int n) {
-        vector<vector<int>> dp (n+1, vector<int>(n+1, INT_MAX));
-        int ans = solRec(1, n, dp);
+        // vector<vector<int>> dp (n+1, vector<int>(n+1, INT_MAX));
+        // int ans = solRec(1, n, dp);
+        int ans = solTab(n);
         return ans;
     }
 };
