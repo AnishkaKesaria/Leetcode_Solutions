230bf66a474a616cb8b1e6079d745e619d64f1da
Anishka <71920305+AnishkaKesaria@users.noreply.github.com>
2023-12-30 13:08:24 +0530

    Time: 25 ms (7.00%), Space: 12.1 MB (68.09%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -7,7 +7,7 @@ public:
             return true;
 
         unordered_map<char,int> m;
-        for(int i=0; i<words.size(); i++)
+        for(int i=0; i<n; i++)
         {
             for(int j=0; j<words[i].size(); j++)
             {
