9ee14b380c3ec85d8778f749ebfc13c7c3104086
Anishka. <71920305+AnishkaKesaria@users.noreply.github.com>
2025-04-06 20:29:46 +0530

    Time: 39 ms (5.04%), Space: 29.5 MB (5.18%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        map<int,int> m;
+        for(auto x: nums)
+            m[x]++;
+        for(int i=0; i<nums.size(); i++) {
+            if(m.find(i)==m.end())
+                return i;
+        }
+        return nums.size();
+    }
+};
