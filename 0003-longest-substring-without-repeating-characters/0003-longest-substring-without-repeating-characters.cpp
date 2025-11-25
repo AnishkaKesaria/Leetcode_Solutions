46bb208e4cf1a40b0b4e9975abd884f794c3ecca
Anishka. <71920305+AnishkaKesaria@users.noreply.github.com>
2025-11-25 08:12:48 +0530

    Time: 14 ms (40.6%), Space: 13.4 MB (38.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        queue<char> q;
-        map<int, int> m;
-        int i =0;
-        int maxLength = 0;
-        while(i!=s.length()) {
-            if(q.empty())
-            {
-                q.push(s[i]);
-                m[s[i]-'a']++;
-            } else {
-                if(m[s[i]-'a']!=0) {
-                    if(q.front()==s[i]) {
-                        q.pop();
-                        q.push(s[i]);
-                    }
-                    else {
-                        int queueSize = q.size();
-                        maxLength = max(maxLength, queueSize);
-                        while(q.front()!=s[i]) {
-                            m[(q.front()-'a')]--;
-                            q.pop();
-                        }
-                        q.pop();
-                        q.push(s[i]);
-                    }
-                } else {
-                    q.push(s[i]);
-                    m[s[i]-'a']++;
+        queue<int> q;
+        unordered_map<char,int> m;
+        int maxLen = 0;
+        for(auto i: s) {
+            if(m.find(i)!=m.end() && m[i]!=0) {
+                int currLen = q.size();
+                maxLen = max(maxLen, currLen);
+                while(q.front()!=i) {
+                    char ch = q.front();
+                    m[ch]--;
+                    q.pop();
                 }
+                q.pop();
+                q.push(i);
+            } else {
+                m[i]++;
+                q.push(i);
             }
-            i++;
         }
-
-        int queueSize = q.size();
-        maxLength = max(maxLength, queueSize);
-        return maxLength;
+        int currLen = q.size();
+        maxLen = max(maxLen, currLen);
+        return maxLen;
     }
 };
