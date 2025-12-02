dcb09d7b33c5e6c9a7a1ff3c4da82f36becae90c
Anishka. <71920305+AnishkaKesaria@users.noreply.github.com>
2025-12-03 01:56:07 +0530

    Time: 0 ms (100%), Space: 94.9 MB (99.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,51 +1,48 @@
 class Solution {
 public:
-    vector<int> mergeArray(vector<int> &nums1, vector<int> &nums2)
-    {
-        vector<int> ansArray;
-        int n1 = nums1.size(), n2 = nums2.size();
-        int i = 0, j = 0;
-        int index = 0;
-        while(i<n1 && j<n2)
-        {
-            if(nums1[i]<nums2[j])
-                ansArray.push_back(nums1[i++]);
-            else
-                ansArray.push_back(nums2[j++]);
-        }
-
-        while(i<n1)
-        {
-            ansArray.push_back(nums1[i++]);
-        }
-
-        while(j<n2)
-        {
-            ansArray.push_back(nums2[j++]);
-        }
-
-        return ansArray;
-    }
-
-    double findMedian(vector<int> &arr)
-    {
-        double median;
-        int n = arr.size();
-        
-        if(n%2==1)
-            median = arr[(n/2)];
-        else
-        {
-            median = (arr[(n/2)-1] + arr[n/2])/2.0;
-        }
-
-        return median;
-    }
-
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double ans;
-        vector<int> arr = mergeArray(nums1, nums2);
-        ans = findMedian(arr);
-        return ans;
+        int n = nums1.size() + nums2.size();
+        int midIndex = n/2;
+        int i=0;
+        int p1=0, p2=0;
+        int ith=0, jth=0;
+        while(i!=midIndex+2) {
+            if(p1<nums1.size()) {
+                if(p2<nums2.size()) {
+                    if(nums1[p1]<nums2[p2]) {
+                        if(i==midIndex)
+                            ith = nums1[p1];
+                        if(i==midIndex-1)
+                            jth = nums1[p1];
+                        p1++;
+                    }
+                    else {
+                        if(i==midIndex)
+                            ith = nums2[p2];
+                        if(i==midIndex-1)
+                            jth = nums2[p2];
+                        p2++;
+                    }    
+                } else {
+                    if(i==midIndex)
+                            ith = nums1[p1];
+                        if(i==midIndex-1)
+                            jth = nums1[p1];
+                    p1++;
+                }
+            } else if (p2<nums2.size()) {
+                if(i==midIndex)
+                    ith = nums2[p2];
+                if(i==midIndex-1)
+                    jth = nums2[p2];
+                p2++; 
+            }
+            i++;
+        }
+
+        if(n%2==0)
+            return ((double)(ith+jth)/2);
+        else
+            return ith;
     }
 };
