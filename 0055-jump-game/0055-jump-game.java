71e6102f91c99979455fd64d343c52d66d62c4d3
Anishka. <71920305+AnishkaKesaria@users.noreply.github.com>
2026-06-19 21:16:58 +0530

    Time: 2 ms (88.77%), Space: 47.7 MB (90.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,36 +1,14 @@
 class Solution {
-    private boolean isPossible(int[] nums, int index, Pair<Boolean, Boolean>[] dp) {
-        if(dp[index].getKey()==true)
-            return dp[index].getValue();
-
-        if(index==nums.length-1)
-            return true;
-        else if (index>nums.length-1)
-            return false;
-        else if (nums[index]==0)
-            return false;
-        else {
-            int maxJump = nums[index];
-            boolean ans = false;
-            int currJump = 1;
-            while(currJump<=maxJump) {
-                ans = (ans || isPossible(nums, index+currJump, dp));
-                currJump++;
-            }
-            dp[index] = new Pair<> (true, ans);
-            return dp[index].getValue();
-        }
-    }
-
     public boolean canJump(int[] nums) {
-        int currIndex = 0;
-        Pair<Boolean, Boolean> dp[] = new Pair[nums.length+1];
-
+        int maxIndex = 0;
         for(int i=0; i<nums.length; i++) {
-            dp[i] = new Pair<> (false, false);
+            if(maxIndex>=i) {
+                maxIndex = Math.max(maxIndex, (i+nums[i]));
+                if(maxIndex>=nums.length-1)
+                    return true;
+            } else 
+                return false;
         }
-
-        boolean ans = isPossible(nums, currIndex, dp);
-        return ans;
+        return false;
     }
 }
